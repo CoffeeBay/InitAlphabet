d7cf98671150621129c18de255c4778ac6f88c70
BangUl_Pro <wind.dong.dream@gmail.com>
2016-12-01 09:27:54 +0900

    initial commit

---
+++ main.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+
+typedef struct {
+    unsigned long b0:1, b1:1, b2:1, b3:1, b4:1, b5:1, b6:1;
+    unsigned long b7:1, b8:1, b9:1, b10:1, b11:1, b12:1, b13:1;
+    unsigned long b14:1, b15:1, b16:1, b17:1, b18:1, b19:1, b20:1;
+    unsigned long b21:1, b22:1, b23:1, b24:1, b25:1, b26:1, b27:1;
+} Alphabet;
+
+Alphabet alphabet[52];
+
+void seta();
+void printAlphabet(Alphabet a);
+
+
+int main() {
+    seta();
+    printAlphabet(alphabet[0]);
+    return 0;
+}
+
+// a
+void seta() {
+    alphabet[0].b0 = 0;
+    alphabet[0].b1 = 1;
+    alphabet[0].b2 = 1;
+    alphabet[0].b3 = 1;
+    alphabet[0].b4 = 1;
+    alphabet[0].b5 = 1;
+    alphabet[0].b6 = 0;
+
+    alphabet[0].b7 = 1;
+    alphabet[0].b8 = 0;
+    alphabet[0].b9 = 0;
+    alphabet[0].b10 = 0;
+    alphabet[0].b11 = 0;
+    alphabet[0].b12 = 0;
+    alphabet[0].b13 = 1;
+
+    alphabet[0].b14 = 0;
+    alphabet[0].b15 = 1;
+    alphabet[0].b16 = 0;
+    alphabet[0].b17 = 0;
+    alphabet[0].b18 = 0;
+    alphabet[0].b19 = 1;
+    alphabet[0].b20 = 0;
+
+    alphabet[0].b21 = 1;
+    alphabet[0].b22 = 1;
+    alphabet[0].b23 = 1;
+    alphabet[0].b24 = 1;
+    alphabet[0].b25 = 1;
+    alphabet[0].b26 = 1;
+    alphabet[0].b27 = 1;
+}
+
+
+
+
+
+
+
+void printAlphabet(Alphabet a) {
+    int i, j;
+    printf("%c", (a.b0 ? '*': ' '));
+    printf("%c", (a.b7 ? '*': ' '));
+    printf("%c", (a.b14 ? '*': ' '));
+    printf("%c", (a.b21 ? '*': ' '));
+    printf("\n");
+
+    printf("%c", (a.b1 ? '*': ' '));
+    printf("%c", (a.b8 ? '*': ' '));
+    printf("%c", (a.b15 ? '*': ' '));
+    printf("%c", (a.b22 ? '*': ' '));
+    printf("\n");
+
+    printf("%c", (a.b2 ? '*': ' '));
+    printf("%c", (a.b9 ? '*': ' '));
+    printf("%c", (a.b16 ? '*': ' '));
+    printf("%c", (a.b23 ? '*': ' '));
+    printf("\n");
+
+    printf("%c", (a.b3 ? '*': ' '));
+    printf("%c", (a.b10 ? '*': ' '));
+    printf("%c", (a.b17 ? '*': ' '));
+    printf("%c", (a.b24 ? '*': ' '));
+    printf("\n");
+
+    printf("%c", (a.b4 ? '*': ' '));
+    printf("%c", (a.b11 ? '*': ' '));
+    printf("%c", (a.b18 ? '*': ' '));
+    printf("%c", (a.b25 ? '*': ' '));
+    printf("\n");
+
+    printf("%c", (a.b5 ? '*': ' '));
+    printf("%c", (a.b12 ? '*': ' '));
+    printf("%c", (a.b19 ? '*': ' '));
+    printf("%c", (a.b26 ? '*': ' '));
+    printf("\n");
+
+    printf("%c", (a.b6 ? '*': ' '));
+    printf("%c", (a.b13 ? '*': ' '));
+    printf("%c", (a.b20 ? '*': ' '));
+    printf("%c", (a.b27 ? '*': ' '));
+    printf("\n");
+}
+

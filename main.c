7defed1bc179172c503a403d099d65d7a0b10a73
BangUl_Pro <wind.dong.dream@gmail.com>
2016-12-01 12:57:08 +0900

    아 커밋명 뭐로하지

--- main.c
+++ main.c
@@ -10,12 +10,45 @@ typedef struct {
 Alphabet alphabet[52];
 
 void seta();
+void setb();
+void setc();
+void setd();
+void sete();
+void setf();
+void setg();
+void seth();
+void seti();
+void setj();
+void setY();
+void setZ();
 void printAlphabet(Alphabet a);
 
 
 int main() {
     seta();
+    setb();
+    setc();
+    setd();
+    sete();
+    setf();
+    setg();
+    seth();
+    seti();
+    setj();
+    setY();
+    setZ();
     printAlphabet(alphabet[0]);
+    printAlphabet(alphabet[1]);
+    printAlphabet(alphabet[2]);
+    printAlphabet(alphabet[3]);
+    printAlphabet(alphabet[4]);
+    printAlphabet(alphabet[5]);
+    printAlphabet(alphabet[6]);
+    printAlphabet(alphabet[7]);
+    printAlphabet(alphabet[8]);
+    printAlphabet(alphabet[9]);
+    printAlphabet(alphabet[50]);
+    printAlphabet(alphabet[51]);
     return 0;
 }
 
@@ -54,7 +87,392 @@ void seta() {
     alphabet[0].b27 = 1;
 }
 
+// b
+void setb() {
+    alphabet[1].b0 = 1;
+    alphabet[1].b1 = 1;
+    alphabet[1].b2 = 1;
+    alphabet[1].b3 = 1;
+    alphabet[1].b4 = 1;
+    alphabet[1].b5 = 1;
+    alphabet[1].b6 = 1;
 
+    alphabet[1].b7 = 0;
+    alphabet[1].b8 = 0;
+    alphabet[1].b9 = 1;
+    alphabet[1].b10 = 0;
+    alphabet[1].b11 = 0;
+    alphabet[1].b12 = 0;
+    alphabet[1].b13 = 1;
+
+    alphabet[1].b14 = 0;
+    alphabet[1].b15 = 0;
+    alphabet[1].b16 = 1;
+    alphabet[1].b17 = 0;
+    alphabet[1].b18 = 0;
+    alphabet[1].b19 = 0;
+    alphabet[1].b20 = 1;
+
+    alphabet[1].b21 = 0;
+    alphabet[1].b22 = 0;
+    alphabet[1].b23 = 0;
+    alphabet[1].b24 = 1;
+    alphabet[1].b25 = 1;
+    alphabet[1].b26 = 1;
+    alphabet[1].b27 = 0;
+}
+
+// c
+void setc() {
+    alphabet[2].b0 = 0;
+    alphabet[2].b1 = 1;
+    alphabet[2].b2 = 1;
+    alphabet[2].b3 = 1;
+    alphabet[2].b4 = 1;
+    alphabet[2].b5 = 1;
+    alphabet[2].b6 = 0;
+
+    alphabet[2].b7 = 1;
+    alphabet[2].b8 = 0;
+    alphabet[2].b9 = 0;
+    alphabet[2].b10 = 0;
+    alphabet[2].b11 = 0;
+    alphabet[2].b12 = 0;
+    alphabet[2].b13 = 1;
+
+    alphabet[2].b14 = 1;
+    alphabet[2].b15 = 0;
+    alphabet[2].b16 = 0;
+    alphabet[2].b17 = 0;
+    alphabet[2].b18 = 0;
+    alphabet[2].b19 = 0;
+    alphabet[2].b20 = 1;
+
+    alphabet[2].b21 = 1;
+    alphabet[2].b22 = 0;
+    alphabet[2].b23 = 0;
+    alphabet[2].b24 = 0;
+    alphabet[2].b25 = 0;
+    alphabet[2].b26 = 0;
+    alphabet[2].b27 = 1;
+}
+
+// d
+void setd() {
+    alphabet[3].b0 = 0;
+    alphabet[3].b1 = 0;
+    alphabet[3].b2 = 0;
+    alphabet[3].b3 = 0;
+    alphabet[3].b4 = 1;
+    alphabet[3].b5 = 1;
+    alphabet[3].b6 = 0;
+
+    alphabet[3].b7 = 0;
+    alphabet[3].b8 = 0;
+    alphabet[3].b9 = 0;
+    alphabet[3].b10 = 1;
+    alphabet[3].b11 = 0;
+    alphabet[3].b12 = 0;
+    alphabet[3].b13 = 1;
+
+    alphabet[3].b14 = 0;
+    alphabet[3].b15 = 0;
+    alphabet[3].b16 = 0;
+    alphabet[3].b17 = 1;
+    alphabet[3].b18 = 0;
+    alphabet[3].b19 = 0;
+    alphabet[3].b20 = 1;
+
+    alphabet[3].b21 = 1;
+    alphabet[3].b22 = 1;
+    alphabet[3].b23 = 1;
+    alphabet[3].b24 = 1;
+    alphabet[3].b25 = 1;
+    alphabet[3].b26 = 1;
+    alphabet[3].b27 = 1;
+}
+
+// e
+void sete() {
+    alphabet[4].b0 = 0;
+    alphabet[4].b1 = 1;
+    alphabet[4].b2 = 1;
+    alphabet[4].b3 = 1;
+    alphabet[4].b4 = 1;
+    alphabet[4].b5 = 1;
+    alphabet[4].b6 = 0;
+
+    alphabet[4].b7 = 1;
+    alphabet[4].b8 = 0;
+    alphabet[4].b9 = 0;
+    alphabet[4].b10 = 1;
+    alphabet[4].b11 = 0;
+    alphabet[4].b12 = 0;
+    alphabet[4].b13 = 1;
+
+    alphabet[4].b14 = 1;
+    alphabet[4].b15 = 0;
+    alphabet[4].b16 = 0;
+    alphabet[4].b17 = 1;
+    alphabet[4].b18 = 0;
+    alphabet[4].b19 = 0;
+    alphabet[4].b20 = 1;
+
+    alphabet[4].b21 = 0;
+    alphabet[4].b22 = 1;
+    alphabet[4].b23 = 1;
+    alphabet[4].b24 = 0;
+    alphabet[4].b25 = 0;
+    alphabet[4].b26 = 0;
+    alphabet[4].b27 = 0;
+}
+
+
+// f
+void setf() {
+    alphabet[5].b0 = 0;
+    alphabet[5].b1 = 0;
+    alphabet[5].b2 = 0;
+    alphabet[5].b3 = 1;
+    alphabet[5].b4 = 0;
+    alphabet[5].b5 = 0;
+    alphabet[5].b6 = 0;
+
+    alphabet[5].b7 = 0;
+    alphabet[5].b8 = 1;
+    alphabet[5].b9 = 1;
+    alphabet[5].b10 = 1;
+    alphabet[5].b11 = 1;
+    alphabet[5].b12 = 1;
+    alphabet[5].b13 = 1;
+
+    alphabet[5].b14 = 1;
+    alphabet[5].b15 = 0;
+    alphabet[5].b16 = 0;
+    alphabet[5].b17 = 1;
+    alphabet[5].b18 = 0;
+    alphabet[5].b19 = 0;
+    alphabet[5].b20 = 0;
+
+    alphabet[5].b21 = 0;
+    alphabet[5].b22 = 0;
+    alphabet[5].b23 = 0;
+    alphabet[5].b24 = 0;
+    alphabet[5].b25 = 0;
+    alphabet[5].b26 = 0;
+    alphabet[5].b27 = 0;
+}
+
+
+// g
+void setg() {
+    alphabet[6].b0 = 0;
+    alphabet[6].b1 = 1;
+    alphabet[6].b2 = 1;
+    alphabet[6].b3 = 0;
+    alphabet[6].b4 = 0;
+    alphabet[6].b5 = 0;
+    alphabet[6].b6 = 0;
+
+    alphabet[6].b7 = 1;
+    alphabet[6].b8 = 0;
+    alphabet[6].b9 = 0;
+    alphabet[6].b10 = 1;
+    alphabet[6].b11 = 0;
+    alphabet[6].b12 = 0;
+    alphabet[6].b13 = 1;
+
+    alphabet[6].b14 = 1;
+    alphabet[6].b15 = 0;
+    alphabet[6].b16 = 0;
+    alphabet[6].b17 = 1;
+    alphabet[6].b18 = 0;
+    alphabet[6].b19 = 0;
+    alphabet[6].b20 = 1;
+
+    alphabet[6].b21 = 0;
+    alphabet[6].b22 = 1;
+    alphabet[6].b23 = 1;
+    alphabet[6].b24 = 1;
+    alphabet[6].b25 = 1;
+    alphabet[6].b26 = 1;
+    alphabet[6].b27 = 0;
+}
+
+// h
+void seth() {
+    alphabet[7].b0 = 1;
+    alphabet[7].b1 = 1;
+    alphabet[7].b2 = 1;
+    alphabet[7].b3 = 1;
+    alphabet[7].b4 = 1;
+    alphabet[7].b5 = 1;
+    alphabet[7].b6 = 1;
+
+    alphabet[7].b7 = 0;
+    alphabet[7].b8 = 0;
+    alphabet[7].b9 = 0;
+    alphabet[7].b10 = 1;
+    alphabet[7].b11 = 0;
+    alphabet[7].b12 = 0;
+    alphabet[7].b13 = 0;
+
+    alphabet[7].b14 = 0;
+    alphabet[7].b15 = 0;
+    alphabet[7].b16 = 0;
+    alphabet[7].b17 = 1;
+    alphabet[7].b18 = 0;
+    alphabet[7].b19 = 0;
+    alphabet[7].b20 = 0;
+
+    alphabet[7].b21 = 0;
+    alphabet[7].b22 = 0;
+    alphabet[7].b23 = 0;
+    alphabet[7].b24 = 1;
+    alphabet[7].b25 = 1;
+    alphabet[7].b26 = 1;
+    alphabet[7].b27 = 1;
+}
+
+// i
+void seti() {
+    alphabet[8].b0 = 0;
+    alphabet[8].b1 = 0;
+    alphabet[8].b2 = 0;
+    alphabet[8].b3 = 0;
+    alphabet[8].b4 = 0;
+    alphabet[8].b5 = 0;
+    alphabet[8].b6 = 0;
+
+    alphabet[8].b7 = 0;
+    alphabet[8].b8 = 0;
+    alphabet[8].b9 = 0;
+    alphabet[8].b10 = 0;
+    alphabet[8].b11 = 0;
+    alphabet[8].b12 = 0;
+    alphabet[8].b13 = 0;
+
+    alphabet[8].b14 = 1;
+    alphabet[8].b15 = 0;
+    alphabet[8].b16 = 1;
+    alphabet[8].b17 = 1;
+    alphabet[8].b18 = 1;
+    alphabet[8].b19 = 1;
+    alphabet[8].b20 = 1;
+
+    alphabet[8].b21 = 0;
+    alphabet[8].b22 = 0;
+    alphabet[8].b23 = 0;
+    alphabet[8].b24 = 0;
+    alphabet[8].b25 = 0;
+    alphabet[8].b26 = 0;
+    alphabet[8].b27 = 0;
+}
+
+// j
+void setj() {
+    alphabet[9].b0 = 0;
+    alphabet[9].b1 = 0;
+    alphabet[9].b2 = 0;
+    alphabet[9].b3 = 0;
+    alphabet[9].b4 = 1;
+    alphabet[9].b5 = 1;
+    alphabet[9].b6 = 0;
+
+    alphabet[9].b7 = 0;
+    alphabet[9].b8 = 0;
+    alphabet[9].b9 = 0;
+    alphabet[9].b10 = 0;
+    alphabet[9].b11 = 0;
+    alphabet[9].b12 = 0;
+    alphabet[9].b13 = 1;
+
+    alphabet[9].b14 = 1;
+    alphabet[9].b15 = 0;
+    alphabet[9].b16 = 1;
+    alphabet[9].b17 = 1;
+    alphabet[9].b18 = 1;
+    alphabet[9].b19 = 1;
+    alphabet[9].b20 = 0;
+
+    alphabet[9].b21 = 0;
+    alphabet[9].b22 = 0;
+    alphabet[9].b23 = 0;
+    alphabet[9].b24 = 0;
+    alphabet[9].b25 = 0;
+    alphabet[9].b26 = 0;
+    alphabet[9].b27 = 0;
+}
+
+// Y
+void setY() {
+    alphabet[50].b0 = 1;
+    alphabet[50].b1 = 1;
+    alphabet[50].b2 = 1;
+    alphabet[50].b3 = 1;
+    alphabet[50].b4 = 0;
+    alphabet[50].b5 = 0;
+    alphabet[50].b6 = 0;
+
+    alphabet[50].b7 = 0;
+    alphabet[50].b8 = 0;
+    alphabet[50].b9 = 0;
+    alphabet[50].b10 = 1;
+    alphabet[50].b11 = 1;
+    alphabet[50].b12 = 1;
+    alphabet[50].b13 = 1;
+
+    alphabet[50].b14 = 1;
+    alphabet[50].b15 = 1;
+    alphabet[50].b16 = 1;
+    alphabet[50].b17 = 1;
+    alphabet[50].b18 = 0;
+    alphabet[50].b19 = 0;
+    alphabet[50].b20 = 0;
+
+    alphabet[50].b21 = 0;
+    alphabet[50].b22 = 0;
+    alphabet[50].b23 = 0;
+    alphabet[50].b24 = 0;
+    alphabet[50].b25 = 0;
+    alphabet[50].b26 = 0;
+    alphabet[50].b27 = 0;
+}
+
+// Z
+void setZ() {
+    alphabet[51].b0 = 1;
+    alphabet[51].b1 = 0;
+    alphabet[51].b2 = 0;
+    alphabet[51].b3 = 0;
+    alphabet[51].b4 = 0;
+    alphabet[51].b5 = 1;
+    alphabet[51].b6 = 1;
+
+    alphabet[51].b7 = 1;
+    alphabet[51].b8 = 0;
+    alphabet[51].b9 = 0;
+    alphabet[51].b10 = 1;
+    alphabet[51].b11 = 1;
+    alphabet[51].b12 = 0;
+    alphabet[51].b13 = 1;
+
+    alphabet[51].b14 = 1;
+    alphabet[51].b15 = 0;
+    alphabet[51].b16 = 1;
+    alphabet[51].b17 = 1;
+    alphabet[51].b18 = 0;
+    alphabet[51].b19 = 0;
+    alphabet[51].b20 = 1;
+
+    alphabet[51].b21 = 1;
+    alphabet[51].b22 = 1;
+    alphabet[51].b23 = 0;
+    alphabet[51].b24 = 0;
+    alphabet[51].b25 = 0;
+    alphabet[51].b26 = 0;
+    alphabet[51].b27 = 1;
+}
 
 
 
@@ -102,6 +520,8 @@ void printAlphabet(Alphabet a) {
     printf("%c", (a.b13 ? '*': ' '));
     printf("%c", (a.b20 ? '*': ' '));
     printf("%c", (a.b27 ? '*': ' '));
+    printf("\n");
+    printf("\n");
     printf("\n");
 }
 
